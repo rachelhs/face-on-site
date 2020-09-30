e0c61897126fe0f32e59d5702db9e4a861b32fd2
rachelhs <rachelsmith400@hotmail.co.uk>
2020-09-30 11:38:44 +0100

    add links to disguises

--- src/components/sections/Disguises.js
+++ src/components/sections/Disguises.js
@@ -7,12 +7,27 @@ const Disguises = () => (
   <Section id="howTo" className={teamStyles.sectionCollective}>
     <Container style={{ position: 'relative' }}>
       <Title><TitleText>DISGUISES</TitleText></Title>
-      <br />
+      <ParagraphText><h2>Here are a few ways to change your face using free browser based apps from the web</h2></ParagraphText>
       <div className={listStyles.spacer}></div>
-        <div className={listStyles.listContainer}><h3>https://reflect.tech/faceswap</h3></div>
-        <div className={listStyles.listContainer}><h3>https://www.deepar.ai/demos</h3></div>
+      <div className={listStyles.listContainer}><a href="https://reflect.tech/faceswap" target="_blank" rel="noopener noreferrer"><h3>Swap faces with a celebrity</h3></a></div>
+        <div className={listStyles.listContainer}><a href="https://sdk.developer.deepar.ai/beautyDemo/index.html" target="_blank" rel="noopener noreferrer"><h3>Add some make up</h3></a></div>
+        <div className={listStyles.listContainer}><a href="https://sdk.developer.deepar.ai/virtualTryOn/index.html" target="_blank" rel="noopener noreferrer"><h3>Add a pair of glasses</h3></a></div>
+        <div className={listStyles.listContainer}><a href="https://sdk.developer.deepar.ai/preview/index.html?e=./effects/gold_face" target="_blank" rel="noopener noreferrer"><h3>Paint your face gold</h3></a></div>
+        <div className={listStyles.listContainer}><a href="https://www.photofunny.net/cat-stickers/photomontage-bearded-man-sticker#logo2" target="_blank" rel="noopener noreferrer"><h3>Grow a beard</h3></a></div>
+        <div className={listStyles.listContainer}><a href="https://changemyface.com/demo.php" target="_blank" rel="noopener noreferrer"><h3>Get older</h3></a></div>
+        <div className={listStyles.listContainer}><a href="https://makeup.pho.to/" target="_blank" rel="noopener noreferrer"><h3>Smooth out wrinkles</h3></a></div>
+        <div className={listStyles.listContainer}><a href="https://sdk.developer.deepar.ai/preview/index.html?e=./effects/bot" target="_blank" rel="noopener noreferrer"><h3>Be a robot</h3></a></div>
     </Container>
   </Section>
 );
 
 export default Disguises;
+
+
+
+
+
+
+
+
+
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -6,9 +6,8 @@ const normalize = `
 
 const GlobalStyles = createGlobalStyle`
   ${normalize};
-  @font-face {font-family: "T-Star"; src: url("//db.onlinewebfonts.com/t/4d6aeef29c4c70c6227ad86b5371f8e9.eot"); src: url("//db.onlinewebfonts.com/t/4d6aeef29c4c70c6227ad86b5371f8e9.eot?#iefix") format("embedded-opentype"), url("//db.onlinewebfonts.com/t/4d6aeef29c4c70c6227ad86b5371f8e9.woff2") format("woff2"), url("//db.onlinewebfonts.com/t/4d6aeef29c4c70c6227ad86b5371f8e9.woff") format("woff"), url("//db.onlinewebfonts.com/t/4d6aeef29c4c70c6227ad86b5371f8e9.ttf") format("truetype"), url("//db.onlinewebfonts.com/t/4d6aeef29c4c70c6227ad86b5371f8e9.svg#T-Star") format("svg"); }
   body {
-    font-family: PxGrotesk-Screen, palatino, arial, sans-serif;
+    font-family: PxGrotesk-Screen, Courier, monospace;
   }
   h1, h2, h3 {
     margin: 0;
@@ -22,7 +21,7 @@ const GlobalStyles = createGlobalStyle`
   }
   
   h1, h2 {
-    font-family: PxGrotesk-Screen, sans-serif;
+    font-family: PxGrotesk-Screen, Courier, monospace;
     font-weight: 700;
     font-style: normal;
   }
